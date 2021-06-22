06bb6693d7a17b1de75b2641656a3fab5e56cef4
Jimena Hernando <jimenahernando@hotmail.com>
2021-06-22 09:27:26 +0200

    Creo la clase Musician

---
+++ 7.Jazz/js/Musician.js
@@ -0,0 +1,8 @@
+export class Musician{
+    constructor(name, instrument, rating){
+        this.name = name;
+        this.instrument = instrument;
+        this.rating = rating;
+        this.id = id;
+    }
+}
